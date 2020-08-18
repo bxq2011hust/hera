e8e0cf721851397e622439da51b044bc238d5cdf
bxq2011hust <bxq2011hust@qq.com>
2020-08-18 13:02:35 +0800

    alloc memory by host

--- src/eei.cpp
+++ src/eei.cpp
@@ -438,7 +438,12 @@ void WasmEngine::collectBenchmarkingData()
 
       m_result.isRevert = revert;
 
-      throw EndExecution{};
+      //throw EndExecution{};
+      //FIXME: throw exception will crash host
+      int value_one = 24;
+      int value_two = 0;
+      int error = value_one / value_two;
+      (void)error;
   }
 
   uint32_t EthereumInterface::eeiGetReturnDataSize()
